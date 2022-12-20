ee772f6e59f5147fcfe56f95d6a1b63da6794802
bhanuprakashkola26 <bhanuprakashkola26@gmail.com>
2022-12-20 23:42:30 +0530

    Assignment added for week 01

---
+++ Assignments/Activity.sh
@@ -0,0 +1,6 @@
+echo "Hello user welcome to the activity room"
+read -p "Enter your first name: " first
+read -p "Enter your last name: " last
+read -p "Enter your Date of Birth: " dob
+echo -e "\nFirstName $first LastName $last"
+echo "\nDate of Birth $dob"
