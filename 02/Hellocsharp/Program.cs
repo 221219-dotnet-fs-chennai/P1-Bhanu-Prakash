65bdcf46b15d3591df1c0411c45791e4c5401ff0
bhanuprakashkola26 <120785261+bhanuprakashkola26@users.noreply.github.com>
2022-12-28 15:35:22 +0530

    Delete 02/Hellocsharp directory

--- 02/Hellocsharp/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Hellocsharp;
-user ob=new user();
-
-ob.Id="576";
-ob.FirstName="Bhanu";
-ob.LastName="kola";
-Console.WriteLine($"Employee Id-{ob.Id} \nFirstName-{ob.FirstName} LastName-{ob.LastName}");
-
-
